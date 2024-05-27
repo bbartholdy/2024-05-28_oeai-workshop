1d658c9d34936962672a992e8263e211e54ebb12
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-05-27 11:43:23 +0200

    gitautosync update 5

--- example-project.R
+++ example-project.R
@@ -35,4 +35,33 @@ mortuary_data$Phase != "disturbed"
 
 arrange(known_context, desc(Length)) # descending order of Length
 
+length(mortuary_data$ID)
+dim(mortuary_data)
+
+mutate(
+  known_context,
+  dataset = "Kewulian Burials"
+)
+
+pit_area <- mutate(
+  known_context,
+  Area = Length * Width,
+  .after = Height
+)
+
+mortuary_data |> # take mortuary_data, and then
+  filter(Phase != "disturbed") |> # filter out "disturbed", and then
+  mutate(Area = Length * Width, .after = Height) # create Area
+
+select(mortuary_data, ID, Length, Width, Height) # select by name
+select(mortuary_data, 1, 3, 4, 5) # select by position
+select(mortuary_data, !c(Layer, Pit, Coffin)) # NOT select by name
+
+select(mortuary_data, !Layer, !Pit, !Coffin) # Does NOT work as expected
+
+dimension_data <- mortuary_data |> # take mortuary_data, and then
+  filter(Phase != "disturbed") |> # filter out "disturbed", and then
+  mutate(Area = Length * Width, .after = Height) |>
+  select(ID, Length, Width, Height, Area, Phase)
+
 data.frame(mortuary_data) # standard data frame
