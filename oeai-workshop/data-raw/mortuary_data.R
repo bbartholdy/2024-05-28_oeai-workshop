4e251758668ddfeb65061d3d4026c27f7382ecd1
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2024-05-27 13:57:22 +0200

    gitautosync update 5

--- oeai-workshop/data-raw/mortuary_data.R
+++ oeai-workshop/data-raw/mortuary_data.R
@@ -8,3 +8,9 @@ library(readxl)
 raw_data <- read_xlsx("data-raw/mortuary-data.xlsx")
 
 str(raw_data)
+
+raw_data |>
+  rename(
+    Height = Hight,
+    IndoPacific_bead = `Indo-Pacific_bead`
+  ) 
